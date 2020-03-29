e2939b7af0b62b8733b3c84748a545adb92fab0e
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-29 12:14:11 +0200

    run a graphql api endpoint using express

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-var { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 // construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
 type Query {
     hello: String
 }`);
 
 // the root provides a resolver function for each API endpoint
 
-var root = {
+const root = {
   hello: () => {
     return "Hello world!";
   }
 };
 
 // run the graphql query '{ hello }' and print out the response
-
-graphql(schema, "{ hello }", root).then(response => {
-  console.log(response);
-});
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);
+console.log(`Running a GraphQL API server at http://localhost:${PORT}/graphql`);
